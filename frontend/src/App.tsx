c8d2fdd9fd20229ef84a47dfbaa06c3f6ff9d976
gigi <123047091+jdkuffa@users.noreply.github.com>
2025-03-29 15:41:36 -0400

    Separate project structure into backend folders

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import { Toaster as Sonner } from "./components/ui/sonner";
+import { TooltipProvider } from "./components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+// import WebcamPage from "./pages/WebcamPage";
+// import ResultsPage from "./pages/ResultsPage";
+import './App.css'
+
+const queryClient = new QueryClient()
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
